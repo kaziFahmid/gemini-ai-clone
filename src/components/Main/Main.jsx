c9a78f5e784d814f32a7835c720e16de70864b2b
kaziFahmid <nirjhorislam42@gmail.com>
2025-01-25 22:30:25 +0600

    chagned

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
     <div className='flex-1 relative'>
 <Header/>
    <div className='max-w-4xl mx-auto' >
-dev branchsadlfjaskf
+devs branchsadlfjaskf
    </div>
    <PromptSearch/>
     </div>
